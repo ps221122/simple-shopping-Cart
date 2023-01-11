c94abd205a8c1fa60f9ee9c56f378c6765879837
thays bell <ps221122@summacollege.com>
2023-01-11 12:41:08 +0100

    simple shopping cart with ajax

--- shoppingCart/resources/views/product.blade.php
+++ shoppingCart/resources/views/product.blade.php
@@ -9,7 +9,7 @@
                     <div class="caption">
                         <h4>{{ $product->name }}</h4>
                         <p>{{ $product->description }}</p>
-                        <p><strong>Price: </strong> {{ $product->price }}$</p>
+                        <p><strong>price: </strong> {{ $product->price }}$</p>
                         <p class="btn-holder"><a href="{{ route('add.to.cart', $product->id) }}"
                                 class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
                     </div>
